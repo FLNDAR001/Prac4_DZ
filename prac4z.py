dd7bb49ea10895544a65324716d22ff72a09c43e
Darfla <darynflanegan@gmail.com>
2018-09-17 21:10:34 +0200

    Defining ADC function and Converting voltage function

--- prac4z.py
+++ prac4z.py
@@ -11,6 +11,20 @@ switch_3 = 27
 switch_4 = 17
 switch_5 = 23
 
+# function to read ADC data from a channel
+def GetData(channel): # channel must be an integer 0-7
+    adc = spi.xfer2([1,(8+channel)<<4,0]) # sending 3 bytes
+    data = ((adc[2]&3) << 8) + adc[1]
+    return data
+
+
+# function to convert data to voltage level (ALSO function for pot)
+# places: number of decimal places needed
+def ConvertVolts(data,places):
+    volts = (data * 3.3) / float(1023)
+    volts = round(volts,places)
+    return volts
+
 
 #GPIO setup
 GPIO.setup(switch_1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
