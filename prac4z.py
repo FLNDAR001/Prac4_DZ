f8cd66804f5a09d83883f66e4b9b0f586ee7b8d2
Jason Zebert <jasonzebert0@gmail.com>
2018-09-17 21:15:57 +0100

    SPI configuration

--- prac4z.py
+++ prac4z.py
@@ -2,7 +2,12 @@
 import os
 import sys
 import RPi.GPIO as GPIO
+import spidev
+import time
 
+spi = spidev.SpiDev() # create spi object
+spi.open(0,0)
+spi.max_speed_hz = 1000000
 
 GPIO.setmode(GPIO.BCM)
 switch_1 = 23
